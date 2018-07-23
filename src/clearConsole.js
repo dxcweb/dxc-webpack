9fbe138310de0d34f1a527b848ee06f926183eef
dxcweb <dxcweb@qq.com>
2018-07-23 15:32:51 +0800

    no message

--- src/clearConsole.js
+++
@@ -1,7 +0,0 @@
-export default function() {
-  if (!process.env.DISABLE_CLEAR_CONSOLE) {
-    process.stdout.write(
-      process.platform === 'win32' ? '\x1B[2J\x1B[0f' : '\x1B[2J\x1B[3J\x1B[H',
-    );
-  }
-}
